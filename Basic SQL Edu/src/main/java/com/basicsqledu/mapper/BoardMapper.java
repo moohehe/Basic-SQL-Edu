1729f59c58000935bfa1b34d63ee5ccbee351e0f
moohe <moohe@WINDOWS-B0O342S>
2018-03-26 16:43:02 +0900

    DB connection test

---
+++ Basic SQL Edu/src/main/java/com/basicsqledu/mapper/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.basicsqledu.mapper;
+
+import java.util.List;
+
+import com.basicsqledu.vo.Board;
+
+
+public interface BoardMapper
+{
+
+	public List<Board> list();
+	public int write(Board board);
+	public int delete(Board board);
+	
+}
---
+++ Basic SQL Edu/src/main/java/com/basicsqledu/vo/Board.java
@@ -0,0 +1,87 @@
+package com.basicsqledu.vo;
+
+public class Board
+{
+	int bnumber;
+	String id;
+	String content;
+	String inputdate;
+	String reinputdate;
+	
+	public Board()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Board(int bnumber, String id, String content, String inputdate, String reinputdate)
+	{
+		super();
+		this.bnumber = bnumber;
+		this.id = id;
+		this.content = content;
+		this.inputdate = inputdate;
+		this.reinputdate = reinputdate;
+	}
+
+	public int getBnumber()
+	{
+		return bnumber;
+	}
+
+	public void setBnumber(int bnumber)
+	{
+		this.bnumber = bnumber;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public String getContent()
+	{
+		return content;
+	}
+
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+
+	public String getInputdate()
+	{
+		return inputdate;
+	}
+
+	public void setInputdate(String inputdate)
+	{
+		this.inputdate = inputdate;
+	}
+
+	public String getReinputdate()
+	{
+		return reinputdate;
+	}
+
+	public void setReinputdate(String reinputdate)
+	{
+		this.reinputdate = reinputdate;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Board [bnumber=" + bnumber + ", id=" + id + ", content=" + content + ", inputdate=" + inputdate
+				+ ", reinputdate=" + reinputdate + "]";
+	}
+	
+	
+	
+	
+}
