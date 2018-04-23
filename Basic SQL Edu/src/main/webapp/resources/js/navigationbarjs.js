c71406d63ce989bc0b246ac22cf2e22706c81576
moohe <moohe@WINDOWS-B0O342S>
2018-04-23 20:15:00 +0900

    asdf

--- Basic SQL Edu/src/main/webapp/resources/js/navigationbarjs.js
+++ Basic SQL Edu/src/main/webapp/resources/js/navigationbarjs.js
@@ -26,11 +26,12 @@ $(function(){
 			});
 		}
 		else if (menu == 'CERTIFICATION') {
-			console.log('certification run'); 
+			console.log('certification run');
 		}
 		else if ($(this).hasClass('submitbtn')) {
+			console.log('submitbtn run');
 			// submit 실행
-			$('#email').submit();
+			$('#faq').submit();
 		}
 		else// (menu == 'CLOSE') {
 		{	console.log('닫기');
